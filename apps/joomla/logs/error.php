a671c7bce0d857ffe5af189bd7330f5088558460
WH <weldon23.henson23@gmail.com>
2014-06-16 08:52:43 -0400

    App 2
    Joomla

---
+++ apps/joomla/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-06-12 16:19:37 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2014-06-12T16:19:37+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
